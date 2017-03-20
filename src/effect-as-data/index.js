5c7cac40bbe2c98c048e501ca240db2afbc54389
psfrankie <frankie-orourke@pluralsight.com>
2017-03-20 08:38:25 -0700

    use run instead of runNode bc its a better example

--- src/effect-as-data/index.js
+++ src/effect-as-data/index.js
@@ -1,7 +1,7 @@
-const { runNode } = require('effects-as-data/node')
+const { run, handlers } = require('effects-as-data/node')
 const { getUser } = require('./get-user')
 
-runNode(getUser, 'female', { onFailure: console.error })
+run(handlers, getUser, 'female', { onFailure: console.error })
 .then(r => r.payload)
 .then(console.log)
 .catch(console.error)
